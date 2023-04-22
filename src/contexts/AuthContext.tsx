9794c912be18992104477f54a5f30737b4c0063e
HugoBianquini <hugobianqui@hotmail.com>
2023-04-21 23:46:02 -0300

    store auth tokens in cookies

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { api } from "@/services/api";
 import { useRouter } from "next/router";
+import { setCookie } from "nookies";
 import { createContext, ReactNode, useState } from "react";
 
 type User = {
@@ -41,6 +42,18 @@ export function AuthProvider({ children }: AuthProviderProps) {
       })
 
       const {token, refreshToken, permissions, roles} = response.data;
+
+      // first param is undefined because signIn() is executed on browser
+      // TODO: change the cookie name
+      setCookie(undefined, 'nextjwt.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
+      
+      setCookie(undefined, 'nextjwt.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
   
       setUser({
         email,
