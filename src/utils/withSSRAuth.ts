08a16a8d95da90e1023d0d0d642c380622c1b051
HugoBianquini <hugobianqui@hotmail.com>
2023-04-28 17:10:11 -0300

    validate users permissions in server-side

--- src/utils/withSSRAuth.ts
+++ src/utils/withSSRAuth.ts
@@ -1,17 +1,45 @@
 import { AuthTokenError } from "@/services/errors/AuthTokenError";
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { parseCookies } from "nookies";
+import decode from 'jwt-decode'
+import { validateUserPermissions } from "./validateUserPermissions";
 
-export function withSSRAuth(fn: GetServerSideProps) {
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+}
+
+export function withSSRAuth(fn: GetServerSideProps, options?: WithSSRAuthOptions) {
   return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<{}>> => {
     const cookies = parseCookies(ctx);
+    const token = cookies['nextjwt.token']
 
     // if user is not authenticated
-    if(!cookies['nextjwt.token']) {
+    if(!token) {
       return {
         redirect: {
           destination: '/',
           permanent: false
+        }
+      }
+    }
+
+    if(options) {
+      const user = decode<{permissions: string[], roles: string[]}>(token);
+      const {permissions, roles} = options
+  
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles
+      })
+
+      if(!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: false
+          }
         }
       }
     }
